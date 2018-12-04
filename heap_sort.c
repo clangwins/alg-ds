7545211b3d1c3bc632cc879be336f8b75503b06e
wuyuxin <yuxin.woo@outlook.com>
2018-12-04 10:26:45 +0800

    Wuyuxin (#9)
    * plan
    
    * 完成选择排序

---
+++ heap_sort.c
@@ -0,0 +1,5 @@
+//
+// Created by 吴裕欣 on 2018/12/3.
+//
+
+#include "heap_sort.h"
---
+++ select_sort.h
@@ -0,0 +1,11 @@
+//
+// Created by 吴裕欣 on 2018/12/3.
+//
+
+#ifndef ALG_DS_SELECT_SORT_H
+#define ALG_DS_SELECT_SORT_H
+
+typedef int ElementType;
+void select_sort(ElementType arr[], int len);
+
+#endif //ALG_DS_SELECT_SORT_H
