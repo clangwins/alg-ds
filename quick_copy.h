4c37c634d131d569bab370c221adb69f51fafe4a
王金贝 <18368831810@139.com>
2018-12-04 11:14:46 +0800

    reproduce quick sort

---
+++ quick_copy.h
@@ -0,0 +1,10 @@
+//
+// Created by william on 2018/12/3.
+//
+
+#ifndef CODE_QUICK_COPY_H
+#define CODE_QUICK_COPY_H
+#include "sort_check.h"
+#include "sort_tool.h"
+typedef int ElementType;
+#endif //CODE_QUICK_COPY_H
