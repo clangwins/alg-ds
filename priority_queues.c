c0ac410f6dce24cc52951b8d162512bd3da14917
王金贝 <18368831810@139.com>
2018-12-09 16:35:07 +0800

    add multiway

--- priority_queues.c
+++ priority_queues.c
@@ -4,31 +4,38 @@
 
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include "priority_queues.h"
 
 ElementType *pq;
+ElementType *qp;
+char **keys;
 unsigned int N = 0;
+char *strings[] = {"z", "c", "d", "a", "t", "q", "y", "h", "l", "q"};
 
 void sink(int k) {
     while (k <= N / 2) {
         int i = 2 * k;
-        int l = less(pq[i], pq[i + 1]) ? i + 1 : i;
-        if (!less(pq[k], pq[l])) break;
-        swap(pq[k], pq[l]);
-        k = l;
+        if (i < N && strcmp(keys[pq[i + 1]], keys[pq[i]]) < 0) i++;
+        if (strcmp(keys[pq[k]], keys[pq[i]]) < 0) break;
+        swap(pq[k], pq[i]);
+        k = i;
     }
 }
 
 void swim(unsigned int k) {
-    while (k > 1 && less(pq[k / 2], pq[k])) {
+    while (k > 1 && strcmp(keys[pq[k]], keys[pq[k / 2]]) < 0) {
         swap(pq[k], pq[k / 2]);
         k /= 2;
     }
 }
-void insert(ElementType v) {
+void insert(ElementType v, char *s) {
     pq[++N] = v;
+    qp[v] = N;
+    keys[v] = s;
     swim(N);
 }
+
 void exch(ElementType *a, ElementType *b) {
     if (*a != *b) {
         *a ^= *b;
@@ -36,35 +43,68 @@ void exch(ElementType *a, ElementType *b) {
         *a ^= *b;
     }
 }
-ElementType delMax() {
+ElementType delMin() {
     ElementType v = pq[1];
     exch(&pq[1], &pq[N--]);
-    pq[N + 1] = 0;
+//    pq[N + 1] = -1; // 不需要 sink方法保证：不会访问N+1
     sink(1);
     return v;
 }
 
-void IndexMaxPQ(ElementType arr[], unsigned int len) {
+int size() {
+    return N;
+}
 
-    pq = calloc(sizeof(NULL), len);
+//增序优先级队列
+void IndexMinPQ() {
+    unsigned int len = sizeof(strings) / sizeof(char *);
+    pq = calloc(len + 1, sizeof(ElementType));
+    qp = calloc(len + 1, sizeof(ElementType));
+    keys = calloc(len + 1, sizeof(char *));
+    char **arr = calloc(len, sizeof(ElementType));
     for (int i = 0; i < len; ++i) {
-        insert(arr[i]);
+        insert(i, strings[i]);
     }
     for (int i = 1; i <= len / 2; ++i) {
-        if (less(pq[i], pq[2 * i]) || (2 * i + 1 <= len && less(pq[i], pq[2 * i + 1]))) {
-            printf("pq is error, i = %d\n", i);
+        if (strcmp(keys[pq[2 * i]], keys[pq[i]]) < 0
+            || (2 * i + 1 <= len && strcmp(keys[pq[2 * i + 1]], keys[pq[i]]) < 0)) {
+            printf("pq is error, i = %d %s %s %s\n", i, keys[i], keys[2 * i], keys[2 * i + 1]);
             break;
         }
     }
     for (int i = 0; i < len; ++i) {
-        arr[len - i - 1] = delMax();
+        arr[i] = keys[delMin()];
     }
-    assert(check_sorted(arr, len));
+    print_s(arr, len);
+}
+
+//模拟多路归并
+void multiway() {
+    char *s1[] = {"a", "b", "c", "d"};
+    char *s2[] = {"a", "d", "d", "g"};
+    char *s3[] = {"b", "d", "e", "z"};
+    int lens[] = {0, 0, 0};
+    char **s[] = {s1, s2, s3};
+    unsigned int num_arr = 3;
+    int arr_ele = 4;
+    pq = calloc(num_arr + 1, sizeof(int));
+    qp = calloc(num_arr + 1, sizeof(int));
+    keys = calloc(num_arr + 1, sizeof(char *));
+    for (int i = 0; i < num_arr; ++i) {
+        insert(i, s[i][0]);
+        lens[i]++;
+    }
+
+    while (size() > 0) {
+        int min = delMin();
+        printf("%s ", keys[min]);
+        if (lens[min] < arr_ele)
+            insert(min, s[min][lens[min]++]);
+    }
+    printf("\n");
 }
 
 int main() {
-    ElementType a[] = {1118, 7, 17, 43, 66, 9, 6, 1, 32, 3, 5, 6, 43, 2, 13, 23, 1, 44, 55, 22, 43, 66, 123, 456, 42};
-
-    IndexMaxPQ(a, sizeof(a) / sizeof(ElementType));
-    print_arr(a, sizeof(a) / sizeof(int));
+    multiway();
+    IndexMinPQ();
 }
