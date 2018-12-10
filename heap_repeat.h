63dde33f6615fb22ed1bda555ecfd0f608fc9e0d
王金贝 <18368831810@139.com>
2018-12-10 14:48:14 +0800

    add heap sort

---
+++ heap_repeat.h
@@ -0,0 +1,10 @@
+//
+// Created by william on 2018/12/10.
+//
+
+#ifndef CODE_HEAP_REPEAT_H
+#define CODE_HEAP_REPEAT_H
+#include "sort_check.h"
+#include "sort_tool.h"
+typedef int ElementType;
+#endif //CODE_HEAP_REPEAT_H
